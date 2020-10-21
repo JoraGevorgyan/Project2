5689f63e210f4ed11d1dc8857bf18bf6c9568a5d
Jora Gevorgyan <joragevorgyan004@gmail.com>
2020-10-21 15:34:51 +0400

    there are any simple changes about templates

--- source/account.cpp
+++ source/account.cpp
@@ -19,38 +19,6 @@ Account::Account(const unsigned& num, const std::string& firstName
      ,balance(balance)
 {
 }
-/*this function will be used for Account's istream operator*/
-template <class type>
-std::pair<type, bool> getNumber(std::string str, type maxSize) {
-    maxSize /= 10;
-    std::pair<type, bool> res(0, true);
-    for(char c : str) {
-        if(c < '0' || c > '9') {
-            if(c != ',') {
-                res.second = false;
-                std::cout << "There's non number symbol, please try again and type number\n";
-                return res;
-            }
-             /*
-              * after ',' symbols are ignored,
-              * in the future I can add here code, that'll convert them to double
-              */
-            std::cout << "symbols after ',' are ignored\n";
-             return res;
-        }
-        if(res.first < maxSize) {
-            res.first = res.first * 10 + c - '0';
-        } else {
-            res.second = false;
-            std::cout << "You have written big number, you can type\n"
-                << "numbers only less then "
-                << maxSize
-                << "\nplease try again\n";
-            return res;
-        }
-    }
-    return res;
-}
 
 /*this function will be used for Account's istream operator */
 std::pair<std::string, bool> getName(std::string str) {
@@ -100,8 +68,7 @@ std::istream& Account::operator >> (std::istream& in) {
     while(true) {
         std::cout << "Enter Account Number: ";
         in >> inputStr;
-        unsigned max = UINT_MAX; // this will help the template(in getNumber) find the desired type
-        std::pair<unsigned, bool> result = getNumber(inputStr, max);
+        std::pair<unsigned, bool> result = getNumber< unsigned >(inputStr, UINT_MAX);
             if(result.second) {
             num = result.first;
             break;
@@ -144,8 +111,7 @@ std::istream& Account::operator >> (std::istream& in) {
     while(true) {
         std::cout << "Enter Balance: ";
         in >> inputStr;
-        ULLong max = ULLONG_MAX; // this will help the template(in getNumber) find the desired type
-        std::pair<ULLong, bool> result = getNumber(inputStr, max);
+        std::pair<ULLong, bool> result = getNumber< ULLong >(inputStr, ULLONG_MAX);
         if(result.second) {
             balance = result.first;
             break;
--- source/bankAccountCtrl.hpp
+++ source/bankAccountCtrl.hpp
@@ -27,7 +27,38 @@ struct  AccountBase : public Account {
         std::vector< int > listofID;
 };
 */
-
+/*this function will be used for Account's istream operator*/
+template <class type>
+std::pair<type, bool> getNumber(std::string str, type maxSize) {
+    maxSize /= 10;
+    std::pair<type, bool> res(0, true);
+    for(char c : str) {
+        if(c < '0' || c > '9') {
+            if(c != '.') {
+                res.second = false;
+                std::cout << "There's non number symbol, please try again and type number\n";
+                return res;
+            }
+             /*
+              * after '.' symbols are ignored,
+              * in the future I can add here code, that'll convert them to double
+              */
+            std::cout << "symbols after '.' are ignored\n";
+             return res;
+        }
+        if(res.first < maxSize) {
+            res.first = res.first * 10 + c - '0';
+        } else {
+            res.second = false;
+            std::cout << "You have written big number, you can type\n"
+                << "numbers only less then "
+                << maxSize
+                << "\nplease try again\n";
+            return res;
+        }
+    }
+    return res;
+}
 
 namespace bank {
     void addRec() {
